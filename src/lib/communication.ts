84700574bcbffefa0df11aa60594dcce5ebc3f42
SidneyArmitage <sichy@live.com.au>
2018-02-10 14:25:57 +1100

    starting to add http/https methods to communication

--- src/lib/communication.ts
+++ src/lib/communication.ts
@@ -14,29 +14,98 @@ import * as aws4 from 'aws4'
 const api_target = 'DynamoDB_20120810'
 const service = 'dynamodb'
 
-interface IawsCredentials {
+export interface IawsCredentials {
   accessKeyId: string,
   secretAccessKey: string
 }
 
-interface IHost {
+export interface IHost {
   region: string,
   domain: string
 }
 
 export interface ICommunicationOptions {
   awsCredentials?: IawsCredentials,
-  Host?: string | IHost
+  host: string | IHost
+  method?: string
+}
+
+interface IAwsOptions {
+  host: string
+  path: string
+  method: string
+  headers: any
+  body: string
 }
 
 export class Communications {
 
+  private options: IAwsOptions
+  private isHTTPs: boolean
+
   constructor (options: ICommunicationOptions) {
-
+    this.options = {} as IAwsOptions
+    if(typeof options.host === 'string'){
+      this.isHTTPs = options.host.match(/https\:\/\//) !== null
+      let host = options
+        .host
+        .substring(this.isHTTPs ? 8 : 7, options.host.length)
+      this.options.host = 
+        host.indexOf('/')  != -1 ? 
+          host.substr(0, host.indexOf('/')) : 
+          host
+      this.options.path = 
+        host.indexOf('/')  != -1 ? 
+          host.substring(host.indexOf('/'), host.length) :
+          '/'
+    }else{
+      this.isHTTPs = true
+      this.options.host = 
+        service + 
+        '.' +
+        options.host.region + 
+        '.' + 
+        options.host.domain
+      this.options.path = '/'
+    }
+    this.options.method = options.method ? options.method : 'POST'
+    this.options.body = '{}'
+    this.options.headers['Content-Type'] = 'application/x-amz-json-1.0'
+    this.options.headers['X-Amz-Target'] = 'DynamoDB_20120810.ListTables'
   }
 
-  public request() {
-  
+  public getHost() {
+    return this.options.host
   }
+
+  public getPath() {
+    return this.options.path
+  }
+
+  public getIsHTTPs(): boolean {
+    return this.isHTTPs
+  }
+
+  public request(): Promise<JSON>{
+    return new Promise<JSON>((resolve, reject) => {
+      let req: http.ClientRequest
+      aws4.sign(this.options)
+      if (this.isHTTPs) {
+        req = http.request(this.options, (res) => {
+          response(res, resolve, reject)
+        })
+      } else {
+        req = https.request(this.options, (res) => {
+          response(res, resolve, reject)
+        })
+      }
+
+      req.on('error', reject)
+    })
+  }
+
+}
+
+function response(res: http.IncomingMessage, resolve: (out: JSON) => void, reject: (out: any) => void) {
 
 }
--- src/tests/communication_spec.ts
+++ src/tests/communication_spec.ts
@@ -3,4 +3,85 @@ import * as communication from '../lib/communication'
 
 describe('communication', () => {
 
+  describe('constructor', () => {
+    
+    it('has a string host and HTTP', () => {
+      let comms = new communication.Communications({
+        host: 'http://127.0.0.1:200'
+      })
+      expect(comms.getHost())
+        .to
+        .equal('127.0.0.1:200')
+      expect(comms.getIsHTTPs())
+        .to
+        .equal(false)
+    })
+
+    it('has a string host and HTTPS', () => {
+      let comms = new communication.Communications({
+        host: 'https://127.0.0.1:200'
+      })
+      expect(comms.getHost())
+        .to
+        .equal('127.0.0.1:200')
+        expect(comms.getIsHTTPs())
+          .to
+          .equal(true)
+    })
+
+    it('has a string host with empty path and HTTP', () => {
+      let comms = new communication.Communications({
+        host: 'http://127.0.0.1:200/'
+      })
+      expect(comms.getHost())
+        .to
+        .equal('127.0.0.1:200')
+      expect(comms.getIsHTTPs())
+        .to
+        .equal(false)
+      expect(comms.getPath())
+        .to
+        .equal('/')
+    })
+
+    it('has a string host with custom path and HTTP', () => {
+      let comms = new communication.Communications({
+        host: 'http://127.0.0.1:200/boo'
+      })
+      expect(comms.getHost())
+        .to
+        .equal('127.0.0.1:200')
+      expect(comms.getIsHTTPs())
+        .to
+        .equal(false)
+      expect(comms.getPath())
+        .to
+        .equal('/boo')
+    })
+
+    it('has an IHost', () => {
+      let comms = new communication.Communications({
+        host: {
+          region: 'test',
+          domain: 'testing'
+        }
+      })
+      expect(comms.getHost())
+        .to
+        .equal('dynamodb.test.testing')
+      expect(comms.getIsHTTPs())
+        .to
+        .equal(true)
+    })
+
+    it('has credentials', () => {
+
+    })
+
+    it('does not have credentials', () => {
+
+    })
+
+  })
+
 })
